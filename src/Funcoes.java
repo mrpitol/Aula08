df448af36d0f9f3c41d86fe880ad0da6ccc50fcf
Marcelo Pitol <mrpitol@outlook.com>
2021-06-19 14:42:02 -0300

    Desafio 1 - função estado

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -8,7 +8,7 @@ public class Funcoes {
 	}
 	// fim da função login
 	
-	
+	//Função Somar Carrinho
 	public static void somarCarrinho(double[] produtos) {
 		double soma = 0;
 		for(int i=0; i<produtos.length;i++) {
@@ -17,12 +17,19 @@ public class Funcoes {
 		
 		System.out.println("O total do carrinho é R$" + soma);
 	}
+	// fim da Função Somar Carrinho
 	
+	//Função exibir Menu
 	public static void exibirMenu() {
 		System.out.println("FIAP STORE! A LOJA DOS FIAPERS!");
 		System.out.println("1 - Informar todos os valores do carrinho");
 		System.out.println("2 - Exibir total do carrinho");
 		System.out.println("3 - Sair");
 	}
+	// Fim da Função exibir Menu
 
-}
+	
+	}
+
+	
+	
